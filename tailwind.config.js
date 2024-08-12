7b8e627b61367b8120311757453bbb8046fe349f
mitantsoa1 <73829570+mitantsoa1@users.noreply.github.com>
2024-08-12 16:21:21 +0300

    Initialisation du projet

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["templates/**/*.html.twig", "assets/scripts/.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
